a677c0f7cb5b3e0110337f5efbf8e66d1381a094
kaminyou ho <kaminyouho@aetherai.com>
2023-03-30 19:59:58 +0000

    part1 finish

---
+++ mp2/kernel/paging.c
@@ -0,0 +1,17 @@
+#include "param.h"
+#include "types.h"
+#include "memlayout.h"
+#include "riscv.h"
+#include "spinlock.h"
+#include "defs.h"
+#include "proc.h"
+
+/* NTU OS 2022 */
+/* Page fault handler */
+int handle_pgfault() {
+  /* Find the address that caused the fault */
+  /* uint64 va = r_stval(); */
+
+  /* TODO */
+  panic("not implemented yet\n");
+}
