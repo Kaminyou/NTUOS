daacc9edef7fbbe2832e282851e8a1aed241139f
kaminyou ho <kaminyouho@aetherai.com>
2023-04-30 19:12:29 +0000

    init mp3

---
+++ mp3/xv6-riscv/kernel/thrd.c
@@ -0,0 +1,67 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+// for mp3
+uint64
+sys_thrdstop(void)
+{
+  int delay;
+  uint64 context_id_ptr;
+  uint64 handler, handler_arg;
+  if (argint(0, &delay) < 0)
+    return -1;
+  if (argaddr(1, &context_id_ptr) < 0)
+    return -1;
+  if (argaddr(2, &handler) < 0)
+    return -1;
+  if (argaddr(3, &handler_arg) < 0)
+    return -1;
+
+  //struct proc *proc = myproc();
+
+  //TODO: mp3
+
+  return 0;
+}
+
+// for mp3
+uint64
+sys_cancelthrdstop(void)
+{
+  int context_id, is_exit;
+  if (argint(0, &context_id) < 0)
+    return -1;
+  if (argint(1, &is_exit) < 0)
+    return -1;
+
+  if (context_id < 0 || context_id >= MAX_THRD_NUM) {
+    return -1;
+  }
+
+  //struct proc *proc = myproc();
+
+  //TODO: mp3
+
+  return 0;
+}
+
+// for mp3
+uint64
+sys_thrdresume(void)
+{
+  int context_id;
+  if (argint(0, &context_id) < 0)
+    return -1;
+
+  //struct proc *proc = myproc();
+
+  //TODO: mp3
+
+  return 0;
+}
---
+++ mp3/xv6-riscv/user/threads_sched.c
@@ -0,0 +1,42 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "user/list.h"
+#include "user/threads.h"
+#include "user/threads_sched.h"
+
+#define NULL 0
+
+/* default scheduling algorithm */
+struct threads_sched_result schedule_default(struct threads_sched_args args)
+{
+    struct thread *thread_with_smallest_id = NULL;
+    struct thread *th = NULL;
+    list_for_each_entry(th, args.run_queue, thread_list) {
+        if (thread_with_smallest_id == NULL || th->ID < thread_with_smallest_id->ID) {
+            thread_with_smallest_id = th;
+        }
+    }
+
+    struct threads_sched_result r;
+    if (thread_with_smallest_id != NULL) {
+        r.scheduled_thread_list_member = &thread_with_smallest_id->thread_list;
+        r.allocated_time = thread_with_smallest_id->remaining_time;
+    } else {
+        r.scheduled_thread_list_member = args.run_queue;
+        r.allocated_time = 1;
+    }
+
+    return r;
+}
+
+/* Earliest-Deadline-First scheduling */
+struct threads_sched_result schedule_edf(struct threads_sched_args args)
+{
+    return schedule_default(args);
+}
+
+/* Rate-Monotonic Scheduling */
+struct threads_sched_result schedule_rm(struct threads_sched_args args)
+{
+    return schedule_default(args);
+}
